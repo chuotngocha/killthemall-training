cf044e956036d93c12d0a8d2ca7b87572930e430
chuotngocha <chuotngocha@gmail.com>
2014-12-15 17:40:43 +0700

    finished

--- src/com/edu4java/android/killthemall/GameView.java
+++ src/com/edu4java/android/killthemall/GameView.java
@@ -1,19 +1,24 @@
 package com.edu4java.android.killthemall;
 
+import java.util.ArrayList;
+import java.util.List;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView {
 
-	private Bitmap bmp;
 	private SurfaceHolder holder;
 	private GameLoopThread gameLoopThread;
-	private Sprite sprite;
+	private List<Sprite> sprites = new ArrayList<Sprite>();
+	private long lastClick;
+	private List<TempSprite> temps = new ArrayList<TempSprite>();;
+	private Bitmap bmpBlood;
 
 	public GameView(Context context) {
 		super(context);
@@ -40,6 +45,7 @@ public class GameView extends SurfaceView {
 
 			@Override
 			public void surfaceCreated(SurfaceHolder holder) {
+				createSprites();
 				gameLoopThread.setRunning(true);
 				gameLoopThread.start();
 			}
@@ -49,15 +55,61 @@ public class GameView extends SurfaceView {
 					int width, int height) {
 			}
 		});
+		bmpBlood = BitmapFactory.decodeResource(getResources(),
+				R.drawable.blood1);
+	}
 
-		bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bad1);
-		sprite = new Sprite(this, bmp);
+	private void createSprites() {
+		sprites.add(createSprite(R.drawable.bad1));
+		sprites.add(createSprite(R.drawable.bad2));
+		sprites.add(createSprite(R.drawable.bad3));
+		sprites.add(createSprite(R.drawable.bad4));
+		sprites.add(createSprite(R.drawable.bad5));
+		sprites.add(createSprite(R.drawable.bad6));
+		sprites.add(createSprite(R.drawable.good1));
+		sprites.add(createSprite(R.drawable.good2));
+		sprites.add(createSprite(R.drawable.good3));
+		sprites.add(createSprite(R.drawable.good4));
+		sprites.add(createSprite(R.drawable.good5));
+		sprites.add(createSprite(R.drawable.good6));
+	}
+
+	private Sprite createSprite(int resouce) {
+		Bitmap bmp = BitmapFactory.decodeResource(getResources(), resouce);
+		return new Sprite(this, bmp);
 	}
 
 	@Override
 	public void draw(Canvas canvas) {
 		canvas.drawColor(Color.BLACK);
-		sprite.draw(canvas);
+
+		for (int i = temps.size() - 1; i >= 0; i--) {
+			temps.get(i).draw(canvas);
+		}
+
+		for (Sprite sprite : sprites) {
+			sprite.draw(canvas);
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (System.currentTimeMillis() - lastClick > 500) {
+			lastClick = System.currentTimeMillis();
+			float x = event.getX();
+			float y = event.getY();
+			synchronized (getHolder()) {
+				for (int i = sprites.size() - 1; i >= 0; i--) {
+					Sprite sprite = sprites.get(i);
+					if (sprite.isCollition(event.getX(), event.getY())) {
+						sprites.remove(sprite);
+						temps.add(new TempSprite(temps, this, x, y, bmpBlood));
+						break;
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 }
--- src/com/edu4java/android/killthemall/Sprite.java
+++ src/com/edu4java/android/killthemall/Sprite.java
@@ -3,6 +3,8 @@
  */
 package com.edu4java.android.killthemall;
 
+import java.util.Random;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -18,36 +20,59 @@ public class Sprite {
 	private int x = 0;
 	private int y = 0;
 	private int xSpeed = 5;
+	private int ySpeed;
 	private GameView gameView;
 	private Bitmap bmp;
 	private int currentFrame = 0;
 	private int width;
 	private int height;
+	private static final int MAX_SPEED = 5;
 
 	public Sprite(GameView gameView, Bitmap bmp) {
 		this.gameView = gameView;
 		this.bmp = bmp;
 		this.width = bmp.getWidth() / BMP_COLUMNS;
 		this.height = bmp.getHeight() / BMP_ROWS;
+		Random rnd = new Random();
+		x = rnd.nextInt(gameView.getWidth() - width);
+		y = rnd.nextInt(gameView.getHeight() - height);
+		xSpeed = rnd.nextInt(MAX_SPEED * 2) - MAX_SPEED;
+		ySpeed = rnd.nextInt(MAX_SPEED * 2) - MAX_SPEED;
 	}
 
 	private void update() {
-		if (x > gameView.getWidth() - this.width - xSpeed) {
-			xSpeed = -10;
-		}
-		if (x + xSpeed < 0) {
-			xSpeed = 10;
+		if (x >= gameView.getWidth() - width - xSpeed || x + xSpeed <= 0) {
+			xSpeed = -xSpeed;
 		}
 		x = x + xSpeed;
+		if (y >= gameView.getHeight() - height - ySpeed || y + ySpeed <= 0) {
+			ySpeed = -ySpeed;
+		}
+		y = y + ySpeed;
 		currentFrame = ++currentFrame % BMP_COLUMNS;
 	}
 
 	public void draw(Canvas canvas) {
 		update();
 		int srcX = currentFrame * this.width;
-		int srcY = 1 * this.height;
+		int srcY = getAnimationRow() * height;
 		Rect src = new Rect(srcX, srcY, srcX + this.width, srcY + this.height);
 		Rect dst = new Rect(x, y, x + this.width, y + this.height);
 		canvas.drawBitmap(bmp, src, dst, null);
 	}
+
+	// direction = 0 up, 1 left, 2 down, 3 right,
+	// animation = 3 up, 1 left, 0 down, 2 right
+	int[] DIRECTION_TO_ANIMATION_MAP = { 3, 1, 0, 2 };
+
+	private int getAnimationRow() {
+		double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+		int direction = (int) Math.round(dirDouble) % BMP_ROWS;
+		return DIRECTION_TO_ANIMATION_MAP[direction];
+	}
+
+	public boolean isCollition(float x2, float y2) {
+		return x2 > x && x2 < x + width && y2 > y && y2 < y + height;
+	}
+
 }
